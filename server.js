949ef6349734b485a70cc09074638ec4963821dc
gabriel-riqueto <127115176+gabriel-riqueto@users.noreply.github.com>
2025-09-19 03:24:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,15 @@ app.use(express.json());
 
 app.get('/health', (req, res) => res.json({ ok: true }));
 
-// Endpoint simples
+// Endpoint 
 app.get('/', (req, res) => {
-  res.send('Olá 👋 App de exemplo para SAST/DAST/SCA.');
+  res.send('Teste de SAST/DAST/SCA.');
 });
 
-// Endpoint intencionalmente "questionável" para demonstração de SAST
+// Endpoint ruim para teste de SAST
 app.post('/echo', (req, res) => {
   const { input } = req.body || {};
-  // ❗ Exemplo didático: nunca use eval na vida real
-  // if (input && input.startsWith('calc:')) {
-  //   return res.json({ result: eval(input.replace('calc:', '')) });
-  // }
+
   return res.json({ you_sent: input || null });
 });
 
